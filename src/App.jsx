d7d524ba2a916f65ad6eae4d31c2c05ae6022f2c
Ali Habibi <alihabibiwork2000@gmail.com>
2023-10-27 12:09:19 +0330

    Create logic func for calculate bmi

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,48 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [weight, setWeight] = useState(0);
+  const [height, setHeight] = useState(0);
+  const [bmi, setBmi] = useState("");
+  const [message, setMessage] = useState("");
+
+  const calcBmi = (e) => {
+    e.preventDefault();
+
+    if (weight === 0 || height === 0) {
+      alert("please enter a valid weight and height");
+    } else {
+      let bmi = (weight / (height * height)) * 703;
+      setBmi(bmi.toFixed(1));
+
+      if (bmi < 25) {
+        setMessage("You are underweight");
+      } else if (bmi >= 25 && bmi < 30) {
+        setMessage("You are a healthy weight");
+      } else {
+        setMessage("You are overweight");
+      }
+    }
+  };
+
+  const reload = () => {
+    window.location.reload();
+  };
+
   return (
     <>
-      <div className="App">
+      <div className="app">
         <div className="container">
           <h2>BMI Calculator</h2>
-          <form>
+          <form onSubmit={calcBmi}>
             <div>
               <label htmlFor="">Weight (ibs)</label>
               <input
                 type="text"
                 placeholder="Enter weight value"
                 value={weight}
+                onChange={(e) => setWeight(e.target.value)}
               />
             </div>
             <div>
@@ -21,6 +51,7 @@ function App() {
                 type="text"
                 placeholder="Enter height value"
                 value={height}
+                onChange={(e) => setHeight(e.target.value)}
               />
             </div>
 
@@ -28,11 +59,7 @@ function App() {
               <button type="submit" className="btn">
                 Submit
               </button>
-              <button
-                type="submit"
-                className="btn btn-outline"
-                onClick={reload}
-              >
+              <button className="btn btn-outline" onClick={reload}>
                 Reload
               </button>
             </div>
